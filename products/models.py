96ce173ee751a2327cd43313f4c01823541137f2
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-24 18:41:35 +0545

    Updated vendor attribute

--- products/models.py
+++ products/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    vendor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
+    vendor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
