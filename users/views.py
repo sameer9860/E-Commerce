8376097176adab055110741ac0acf320e0f418b0
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-24 18:45:20 +0545

    Created userviewset

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CustomUser
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
