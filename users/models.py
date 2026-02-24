79a20f5bad4eeac45fd99574bb378d521ba0df8b
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-24 18:36:07 +0545

    Created customuser vendor and customer

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('vendor', 'Vendor'),
+        ('customer', 'Customer'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='customer')
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
