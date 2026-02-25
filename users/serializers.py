256460eb9e756fdd0f8aece90d94fa65012b0c0c
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-25 19:14:11 +0545

    Seperated customer and vendor

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import CustomUser
 
-class UserSerializer(serializers.ModelSerializer):
+class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ["id", "username", "email", "role"]
+        extra_kwargs = {"role": {"default": "vendor"}}
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["id", "username", "email", "role"]
+        extra_kwargs = {"role": {"default": "customer"}}
