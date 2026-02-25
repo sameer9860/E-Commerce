c728b9ca92f4af3933e38a6f877b29203b189d8c
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-25 19:17:56 +0545

    Updated route for vendor and customer

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import VendorViewSet, CustomerViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'vendors', VendorViewSet, basename="vendor")
+router.register(r'customers', CustomerViewSet, basename="customer")
 
 urlpatterns = router.urls
