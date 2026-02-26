438054eb39ee1e03032326f52198551464fe64d1
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-26 20:19:39 +0545

    Restrict only vendor can add products

--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        if user.role != "vendor":
+            raise PermissionDenied("Only vendors can add products.")
+        serializer.save(vendor=user)
