2855340a58eef1a078af12136cc9d2b0377a7e18
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-27 20:24:17 +0545

    Created orderviewset

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
+from .models import Order
+from .serializers import OrderSerializer
 
-# Create your views here.
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        if user.role != "customer":
+            raise PermissionDenied("Only customers can place orders.")
+        serializer.save(customer=user)
