2117b170f94f13fa6cd22c4a389e6a524e146857
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-27 20:25:26 +0545

    Added order url

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
     path('api/', include('users.urls')),
+    path('api/', include('orders.urls')),
     # JWT endpoints
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
