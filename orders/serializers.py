7e52d27a5173f951d7f32e84bef8c98c3bef1396
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-28 19:31:18 +0545

    Added cartitem

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, Cart, CartItem
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = "__all__"
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ["id", "product", "quantity"]
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ["id", "customer", "items", "created_at"]
