8411af1adc10820d5d6fc947110bfda3ee5f32ea
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-28 19:32:36 +0545

    Added cart and cartitem

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, permissions
 from rest_framework.exceptions import PermissionDenied
-from .models import Order
-from .serializers import OrderSerializer
+from .models import Order, Cart, CartItem
+from .serializers import OrderSerializer, CartSerializer, CartItemSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
@@ -13,3 +13,25 @@ class OrderViewSet(viewsets.ModelViewSet):
         if user.role != "customer":
             raise PermissionDenied("Only customers can place orders.")
         serializer.save(customer=user)
+
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        if user.role != "customer":
+            raise PermissionDenied("Only customers can have carts.")
+        serializer.save(customer=user)
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        cart = Cart.objects.get(customer=self.request.user)
+        serializer.save(cart=cart)
