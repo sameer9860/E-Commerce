4e9332b7bcdf4825afd34b338ab39aa1d60f69a3
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-28 19:29:55 +0545

    Added Cart model

--- orders/models.py
+++ orders/models.py
@@ -23,3 +23,25 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} by {self.customer.username}"
+
+
+
+
+class Cart(models.Model):
+    customer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="cart"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart {self.id} for {self.customer.username}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
