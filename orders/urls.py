d0b922b65e7159be0093e5beeed66093ea3791ff
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-28 19:33:54 +0545

    Added cart and cart item

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet
+from .views import OrderViewSet, CartViewSet, CartItemViewSet
 
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet)
+router.register(r'carts', CartViewSet) 
+router.register(r'cart-items', CartItemViewSet)
 
 urlpatterns = router.urls
+
+
