62176aaccedb8ad53e8fd88ae121ee4433ecca5b
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-01 13:17:41 +0545

    Added Paymentt model

--- orders/models.py
+++ orders/models.py
@@ -45,3 +45,15 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
+    
+    
+
+class Payment(models.Model):
+    order = models.OneToOneField("Order", on_delete=models.CASCADE, related_name="payment")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, default="Pending")  # Pending, Success, Failed
+    esewa_transaction_id = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"Payment for Order {self.order.id} - {self.status}"
+    
