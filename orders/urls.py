2be75aee1a0acf47bcfb1dea54adb7f0b7d9177d
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-01 13:21:27 +0545

    Added esewa routes

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet, CartViewSet, CartItemViewSet
 
+from django.urls import path
+from .views import initiate_esewa_payment, esewa_success, esewa_failure
+
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet)
 router.register(r'carts', CartViewSet) 
@@ -9,3 +12,9 @@ router.register(r'cart-items', CartItemViewSet)
 urlpatterns = router.urls
 
 
+
+urlpatterns = [
+    path("payment/<int:order_id>/", initiate_esewa_payment, name="initiate_esewa_payment"),
+    path("payment/success/", esewa_success, name="esewa_success"),
+    path("payment/failure/", esewa_failure, name="esewa_failure"),
+]
