2960321d8b47e2ef607b3ea93befb81206042660
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-03 13:53:36 +0545

    Updated esewa urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet, CartViewSet, CartItemViewSet
+from .views import OrderViewSet, CartViewSet, CartItemViewSet, PaymentViewSet, VendorAnalyticsView
 
 from django.urls import path
 from .views import initiate_esewa_payment, esewa_success, esewa_failure
@@ -8,9 +8,11 @@ router = DefaultRouter()
 router.register(r'orders', OrderViewSet)
 router.register(r'carts', CartViewSet) 
 router.register(r'cart-items', CartItemViewSet)
+router.register(r'payments', PaymentViewSet, basename="payments")
 
 urlpatterns = router.urls + [
-    path("payment/<int:order_id>/", initiate_esewa_payment, name="initiate_esewa_payment"),
+    path("payment/initiate/", initiate_esewa_payment, name="initiate-esewa-payment"),
     path("payment/success/", esewa_success, name="esewa-success"),
     path("payment/failure/", esewa_failure, name="esewa-failure"),
+    path("vendor/analytics/", VendorAnalyticsView.as_view(), name="vendor-analytics"),
 ]
