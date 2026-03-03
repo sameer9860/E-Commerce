ae168c124059c6a595058c63c1b3f943d658d4fc
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-03 13:58:17 +0545

    Updated with add models

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import CustomUser
 
-# Register your models here.
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "role", "is_approved", "is_staff", "is_active")
+    list_filter = ("role", "is_approved", "is_staff", "is_active")
+    search_fields = ("username", "email")
